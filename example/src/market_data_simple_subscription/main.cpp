7881a8cc1f18cb054e0db1e7d983f4ed2fb3ade3
xtuyaowu <toyaowu@gmail.com>
2022-08-31 16:22:27 +0800

    mult symbols test

--- example/src/market_data_simple_subscription/main.cpp
+++ example/src/market_data_simple_subscription/main.cpp
@@ -5,7 +5,7 @@ class MyEventHandler : public EventHandler {
  public:
   bool processEvent(const Event& event, Session* session) override {
     if (event.getType() == Event::Type::SUBSCRIPTION_DATA) {
-//      std::cout << "  " + toString(event) << std::endl;
+      std::cout << "  " + toString(event) << std::endl;
       for (const auto& message : event.getMessageList()) {
         std::cout << std::string("Best bid and ask at ") + UtilTime::getISOTimestamp(message.getTime()) + " are:" << std::endl;
         for (const auto& element : message.getElementList()) {
@@ -44,13 +44,22 @@ int main(int argc, char** argv) {
   MyEventHandler eventHandler;
   Session session(sessionOptions, sessionConfigs, &eventHandler);
 
+  std::vector<Subscription> subscription;
   // 1、orderbook
-   Subscription subscription("okcoin", "BTC-USDT", "MARKET_DEPTH", "MARKET_DEPTH_MAX=20");
-//    Subscription subscription("coinbase", "BTC-USD", "MARKET_DEPTH", "MARKET_DEPTH_MAX=20");
-  // 2、trade
-  // Subscription subscription("coinbase", "BTC-USD", "TRADE");
-  // 3、kline
-//  Subscription subscription("coinbase", "BTC-USD", "TRADE", "CONFLATE_INTERVAL_MILLISECONDS=300&CONFLATE_GRACE_PERIOD_MILLISECONDS=0");
+   Subscription subscription1("gemini", "BTCUSD", "MARKET_DEPTH", "MARKET_DEPTH_MAX=20","BTCUSD|orderbook");
+   Subscription subscription2("gemini", "ETHUSD", "MARKET_DEPTH", "MARKET_DEPTH_MAX=20","ETHUSD|orderbook");
+
+   // 2、trade
+   Subscription subscription3("gemini", "BTCUSD", "TRADE","", "BTCUSD|trade");
+   Subscription subscription4("gemini", "ETHUSD", "TRADE", "","ETHUSD|trade");
+
+   // 3、kline
+   // Subscription subscription("coinbase", "BTC-USD", "TRADE", "CONFLATE_INTERVAL_MILLISECONDS=300&CONFLATE_GRACE_PERIOD_MILLISECONDS=0");
+
+   subscription.push_back(subscription1);
+   subscription.push_back(subscription2);
+   subscription.push_back(subscription3);
+   subscription.push_back(subscription4);
 
   session.subscribe(subscription);
   while(true)
